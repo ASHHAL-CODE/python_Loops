ca322739d72355ba1d41b27cd6037085a114d196
ASHHAL-CODE <ashhalahmed6@gmail.com>
2024-08-22 23:32:38 +0500

    Add files via upload

---
+++ 2nd.py
@@ -0,0 +1,29 @@
+# for loop
+# for i in range(1000000):
+#     i="hellow world"
+#     print(i)
+
+
+# while loop
+# i=1
+# while i<6:
+#     print(i)
+#     if(i==5):
+#         break
+#     i+=1
+
+
+# tables in python
+# num=int(input("enter your number"))
+# print (f"{num} ka table")
+# for i in range(1,11):
+#     result=num*i
+#     print(f"{num}x{i}={result}")
+
+
+
+
+
+
+
+
---
+++ loops.py
@@ -0,0 +1,25 @@
+# for loops
+
+# for i in range(1,11):
+#     print(i)
+
+# while loop
+# i=1
+# while 1<6:
+#     print(i)
+#     if(i==3):
+#         break
+#     i+=1
+
+# tables in python
+# num=int(input("enter your number"))
+# print (f"{num} ka table")
+# for i in range(1,100):
+#     result=num*i
+#     print(f"{num}x{i}={result}")
+
+num=int(input("ENTER THE NUMBER YOU WANT TO CHECK WHETHER IT IS ODD OR EVEN "))
+if num%2==0: 
+    print(f"{num} is even ")
+else:
+    print(f"{num}is odd ")
